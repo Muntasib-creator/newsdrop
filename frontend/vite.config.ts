7937dc35c352aaf9ffec9f31d917e8c249cee5da
test <muntasibchowdhury@gmail.com>
2025-04-18 20:19:42 +0600

    docker frontend build fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // Make Vite accessible from outside the container
+    port: 3000,
+  },
 })
